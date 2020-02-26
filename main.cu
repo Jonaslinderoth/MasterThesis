f2781eb9e6395c2c09bead98bf2c1c65688a27cf
MoreNope <mlanghave@gmail.com>
2020-02-26 16:39:23 +0100

    things work

--- main.cu
+++ main.cu
@@ -9,7 +9,33 @@
 #include <vector>
 int main ()
 {
+	std::vector<std::vector<float>*>* data = new std::vector<std::vector<float>*>;
+	std::default_random_engine generator;
+	generator.seed(100);
+	std::uniform_real_distribution<double> distribution(50000.0,500000.0);
+	std::uniform_real_distribution<double> distribution2(5.0,20.0);
 
+	for(float i = 0; i < 20; i++){
+		std::vector<float>* point1 = new std::vector<float>{i};
+		for(int i = 0; i < 5; i++){
+			point1->push_back(distribution(generator));
+		}
+		point1->push_back(distribution2(generator));
+		data->push_back(point1);
+
+	}
+
+	DOCGPU d = DOCGPU(data);
+	d.setSeed(1);
+	d.setAlpha(0.1);
+	d.setBeta(0.25);
+	d.setWidth(15);
+	auto res = d.findCluster();
+
+	std::cout << "is 7: " << res.second->size() << std::endl;
+	std::cout << "is 20: " << res.first->size() << std::endl;
+
+	/*
 	std::vector<std::vector<float>*>* data = new std::vector<std::vector<float>*>;
 		{
 			auto point = new std::vector<float>{1,1000};
@@ -33,6 +59,6 @@ int main ()
 		std::cout << "is 4: " << res.first->size() << std::endl;
 
 		std::cout << "is 2: " << res.second->size() << std::endl;;
-
+	*/
 
 }
