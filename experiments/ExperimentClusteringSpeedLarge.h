f010b7b85a75fcddb2ff4f5b19cb9337b5828991
JonasLinderoth <jonaslinderoth@icloud.com>
2020-05-25 16:31:48 +0200

    bug

--- experiments/ExperimentClusteringSpeedLarge.h
+++ experiments/ExperimentClusteringSpeedLarge.h
@@ -3,7 +3,7 @@
 
 #include "Experiment.h"
 
-class ExperimentClusteringSpeed: public Experiment{
+class ExperimentClusteringSpeedLarge: public Experiment{
  public:
  ExperimentClusteringSpeedLarge(std::string name, std::string dir): Experiment(0, name, dir, "Number of Clusters, Number of Points, Dim, Dim in Cluster, Version, Time, Clusters Found, Accuracy"){
 		//this->addTests(numberOfTests);
